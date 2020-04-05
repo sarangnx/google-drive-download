bfc324540f7a8d364829721a4d62090474070154
Sarang <sarang98divakaran@gmail.com>
2020-04-05 12:37:56 +0530

    prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,70 +1,70 @@
 const colors = require('vuetify/es5/util/colors').default
 
 module.exports = {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
+    mode: 'universal',
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend (config, ctx) {
+    head: {
+        titleTemplate: '%s - ' + process.env.npm_package_name,
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+    /*
+    ** Global CSS
+    */
+    css: [
+    ],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+    ],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+        '@nuxtjs/vuetify',
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {
+    },
+    /*
+    ** vuetify module configuration
+    ** https://github.com/nuxt-community/vuetify-module
+    */
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
     }
-  }
 }
