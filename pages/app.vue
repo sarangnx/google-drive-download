023603ce08ec29e3a8e39a5771fa9a787d75438a
Sarang <sarang98divakaran@gmail.com>
2020-04-05 21:30:38 +0530

    login button

--- pages/app.vue
+++ pages/app.vue
@@ -1,7 +1,32 @@
 <template>
     <v-col>
         <div class="d-flex justify-center align-center fill-height">
-            <v-btn>Sign In With Google</v-btn>
+            <v-btn
+                v-show="!authorized"
+                href="/auth/generate"
+                target="_blank"
+            >
+                Sign In With Google
+            </v-btn>
         </div>
     </v-col>
 </template>
+
+<script>
+export default {
+    name: 'app',
+    data: () => ({
+        authorized: false,
+    }),
+    methods:{
+        isAuthorized() {
+            const { auth } = this.$route.query;
+
+            this.authorized = auth === 'true' ? true : false;
+        }
+    },
+    mounted() {
+        this.isAuthorized();
+    }
+}
+</script>
