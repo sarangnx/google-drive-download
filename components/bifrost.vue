458bb2b73c074e351750e37f0272528f4d45bd49
Sarang <sarang98divakaran@gmail.com>
2020-04-10 11:13:25 +0530

    Added bifrost component for logo svg

---
+++ components/bifrost.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="__bifrost">
+        <svg width="829" height="564" viewBox="0 0 829 564" fill="none" xmlns="http://www.w3.org/2000/svg" class="__svg">
+            <g filter="url(#filter0_d)">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M650 220C649.267 220 648.535 220.005 647.804 220.014C634.924 141.724 566.937 82 485 82C463.248 82 442.479 86.2092 423.463 93.8575C389.831 41.0385 330.757 6 263.5 6C158.842 6 74 90.842 74 195.5C74 213.237 76.4369 230.405 80.9942 246.688C35.8443 276.129 6 327.079 6 385C6 470.387 70.86 540.625 154 549.135V550H171H650H664V549.414C748.574 542.308 815 471.411 815 385C815 293.873 741.127 220 650 220Z" fill="white"/>
+            </g>
+            <defs>
+                <filter id="filter0_d" x="0" y="0" width="829" height="564" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                    <feOffset dx="4" dy="4"/>
+                    <feGaussianBlur stdDeviation="5"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                </filter>
+            </defs>
+        </svg>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.__svg {
+    max-width: 100%;
+}
+</style>
