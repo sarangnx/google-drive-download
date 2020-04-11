4ebfdce8df6ca8fdd632ed28e068388223d471ce
Sarang <sarang98divakaran@gmail.com>
2020-04-11 12:13:01 +0530

    Added bifrost component

--- pages/home.vue
+++ pages/home.vue
@@ -1,16 +1,31 @@
 <template>
-    <v-col cols="12" ref="fullheight">
-        <div class="d-flex justify-center align-center fill-height">
-            Home
+    <v-col cols="12" ref="fullheight" class="sky">
+        <div class="d-flex justify-center align-center fill-height flex-column flex-md-row">
+            <div class="__columns d-flex justify-center align-center">
+                <bifrost/>
+            </div>
+            <div class="__columns">
+            </div>
         </div>
     </v-col>
 </template>
 
 <script>
 import fullHeight from '@/mixins/fullHeight';
+import Bifrost from '@/components/bifrost';
 
 export default {
     name: 'home',
     mixins: [fullHeight],
+    components: {
+        Bifrost,
+    }
 }
 </script>
+
+<style scoped lang="scss">
+.__columns {
+    height: 100%;
+    width: 100%;
+}
+</style>
