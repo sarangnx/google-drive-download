44c9fcb28257d8e5ee5c2957f8e71b088cb169dc
Sarang <sarang98divakaran@gmail.com>
2020-04-17 18:44:34 +0530

    Added Notifications base container

---
+++ components/Notifications/Notifications.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="notifications">
+        <slide-y-up-transition :duration="transitionDuration" group mode="out-in">
+            <notification
+                v-for="notification in notifications"
+                v-bind="notification"
+                :clickHandler="notification.onClick"
+                :key="notification.timestamp.getTime()"
+                @close="remove(notification.timestamp)"
+            ></notification>
+        </slide-y-up-transition>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import { SlideYUpTransition } from 'vue2-transitions';
+import Notification from './Notification.vue';
+
+export default {
+    components: {
+        SlideYUpTransition,
+        Notification
+    },
+    props: {
+        transitionDuration: {
+            type: Number,
+            default: 200
+        },
+    },
+    computed: {
+        ...mapGetters({
+            notifications: 'list',
+        }),
+    },
+    methods: {
+        ...mapActions([
+            'remove'
+        ]),
+    },
+};
+</script>
